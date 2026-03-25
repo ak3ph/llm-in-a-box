adfe0b00979bb48a8c7f3030a9a509c3a9ef3771
ak3ph <yashthakur1915@gmail.com>
2026-03-25 17:12:32 +0530

    change log: - v1 of the application - added gitignore from the github gitignore repo

--- app/main.py
+++ app/main.py
@@ -1,22 +1,56 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import requests
 
 app = FastAPI()
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
 
+class PromptRequest(BaseModel):
+    prompt: str
+
 @app.get("/")
 def home():
     return {"message": "LLM API running"}
 
+# @app.post("/generate")
+# def generate(request: PromptRequest):
+#     response = requests.post(
+#         OLLAMA_URL,
+#         json={
+#             "model": "llama3",
+#             "prompt": request.prompt,
+#             "stream": False
+#         }
+#     )
+
+#     data = response.json()
+
+#     return {
+#         "prompt": request.prompt,
+#         "response": data.get("response", "")
+#     }
+
 @app.post("/generate")
-def generate(prompt: str):
-    response = requests.post(
-        OLLAMA_URL,
-        json={
-            "model": "llama3",
-            "prompt": prompt,
-            "stream": False
+def generate(request: PromptRequest):
+    try:
+        response = requests.post(
+            OLLAMA_URL,
+            json={
+                "model": "llama3",
+                "prompt": request.prompt,
+                "stream": False,
+                "options": {
+                    "num_predict": 100
+                }
+            }
+        )
+
+        data = response.json()
+
+        return {
+            "response": data.get("response", "")
         }
-    )
-    return response.json()
+
+    except Exception as e:
+        return {"error": str(e)}
